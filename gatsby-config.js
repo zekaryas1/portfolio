003ee6ec1f4042dbf30e63ba3ef7e48069a7ec87
zekaryas <zekaryasdinku@gmail.com>
2022-12-19 15:15:58 +0300

    - changed site url in gatsby-config.js - change demo website banner

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Zekaryas Tadele',
     description:
       'Zekaryas Tadele is a software engineer who specializes in building (and occasionally designing) exceptional Software applications.',
-    siteUrl: '', // No trailing slash allowed!
+    siteUrl: 'https://github.com/zekaryas1', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@zekaryas1',
   },
